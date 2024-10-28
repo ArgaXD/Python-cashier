5a81e8b3d98d5ad5c9512999ff3688fd7c313b02
ArgaXD <argaslaughter@gmail.com>
2024-10-28 19:52:39 +0700

    Add files via upload

---
+++ cashier.py
@@ -0,0 +1,36 @@
+import time
+current_time = time. time()
+time_now = time.ctime(current_time)
+print (time_now)
+
+data = input("nama pelanggan: ")
+print("======CAFE======")
+print("Americano..........$4")
+print("Caramel latte......$6")
+print("milk...............$2")
+print("Chocolate..........$3")
+
+operator = input("add with (+): ")
+num1 = float(input("harga item 1st: "))
+num2 = float(input("harga item 2nd: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
+    
+
+
+#still unfinished lmao 
+ #Kaprika
+ #i made this just for speedrun
